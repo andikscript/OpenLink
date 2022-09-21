6937961d8337bdb9936ce43556e196181f0e6f8a
andik.saputro <andik.saputro@pactindo.com>
2022-09-21 15:12:23 +0700

    refactor code and fix

--- src/main/java/com/andikscript/igate/controller/MobileController.java
+++ src/main/java/com/andikscript/igate/controller/MobileController.java
@@ -1,7 +1,9 @@
 package com.andikscript.igate.controller;
 
 import com.andikscript.igate.access.PackagerAccess;
+import com.andikscript.igate.service.mobile.MobileService;
 import com.andikscript.igate.util.DecodeIso;
+import com.andikscript.igate.util.PrintIso;
 import com.andikscript.igate.value.Direction;
 import com.andikscript.igate.value.Module;
 import org.springframework.http.HttpStatus;
@@ -12,24 +14,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/mobile")
 public class MobileController {
 
-    private final DecodeIso decodeIso;
+    private final MobileService mobileService;
 
-    public MobileController(DecodeIso decodeIso) {
-        this.decodeIso = decodeIso;
-    }
-
-    @GetMapping(value = "/transfer", produces = "application/json")
-    public ResponseEntity<?> transfer() {
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body("Ini transfer");
+    public MobileController(MobileService mobileService) {
+        this.mobileService = mobileService;
     }
 
     @PostMapping(value = "/transfer/{value}")
     public ResponseEntity<?> postTransfer(@PathVariable(value = "value") String message) {
-        decodeIso.printIsoMessage(decodeIso.parseIsoMessage(message, PackagerAccess.ib), Direction.io, Module.ib);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body("OK");
+                .body(mobileService.transfer(message));
     }
 }
