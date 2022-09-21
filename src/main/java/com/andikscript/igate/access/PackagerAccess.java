bafb9d660297a83feb8481071fdbfaa7c81d8cb5
andik.saputro <andik.saputro@pactindo.com>
2022-09-21 14:55:42 +0700

    add path for access packager

---
+++ src/main/java/com/andikscript/igate/access/PackagerAccess.java
@@ -0,0 +1,8 @@
+package com.andikscript.igate.access;
+
+public class PackagerAccess {
+
+    public static final String ib = "/packager/mobile-ib/iso-packager-ib.xml";
+
+    public static final String itm = "/packager/woori-itm/iso-packager-itm.xml";
+}
