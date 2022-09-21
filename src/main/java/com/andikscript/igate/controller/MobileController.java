308537fc70a322d42add1fd0086a06e29993662e
andik.saputro <andik.saputro@pactindo.com>
2022-09-21 14:56:14 +0700

    add controller for mobile

--- src/main/java/com/andikscript/igate/controller/MobileController.java
+++ src/main/java/com/andikscript/igate/controller/MobileController.java
@@ -1,4 +1,35 @@
 package com.andikscript.igate.controller;
 
+import com.andikscript.igate.access.PackagerAccess;
+import com.andikscript.igate.util.DecodeIso;
+import com.andikscript.igate.value.Direction;
+import com.andikscript.igate.value.Module;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/mobile")
 public class MobileController {
+
+    private final DecodeIso decodeIso;
+
+    public MobileController(DecodeIso decodeIso) {
+        this.decodeIso = decodeIso;
+    }
+
+    @GetMapping(value = "/transfer", produces = "application/json")
+    public ResponseEntity<?> transfer() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Ini transfer");
+    }
+
+    @PostMapping(value = "/transfer/{value}")
+    public ResponseEntity<?> postTransfer(@PathVariable(value = "value") String message) {
+        decodeIso.printIsoMessage(decodeIso.parseIsoMessage(message, PackagerAccess.ib), Direction.io, Module.ib);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("OK");
+    }
 }
