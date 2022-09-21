def7fd9860c10944bb9cb2da665f3b098fb5bc39
andik.saputro <andik.saputro@pactindo.com>
2022-09-21 12:54:19 +0700

    initial commit

---
+++ src/main/java/com/andikscript/igate/IgateApplication.java
@@ -0,0 +1,13 @@
+package com.andikscript.igate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andikscript/igate/IgateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andikscript.igate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
