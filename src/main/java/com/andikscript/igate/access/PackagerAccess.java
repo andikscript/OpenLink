60eeb031d71e2b8e968001f85ca3aed6daae3852
andik.saputro <andik.saputro@pactindo.com>
2022-10-01 10:29:36 +0700

    add packager access core, iacss, atm, global

--- src/main/java/com/andikscript/igate/access/PackagerAccess.java
+++ src/main/java/com/andikscript/igate/access/PackagerAccess.java
@@ -5,4 +5,12 @@ public class PackagerAccess {
     public static final String ib = "/packager/mobile-ib/iso-packager-ib.xml";
 
     public static final String itm = "/packager/woori-itm/iso-packager-itm.xml";
+
+    public static final String atm = "/packager/atm/iso-packager-atm.xml";
+
+    public static final String iacss = "/packager/iacss/iso-packager-iacss-web.xml";
+
+    public static final String core = "/packager/core/iso-packager-core.xml";
+
+    public static final String global = "/packager/global/iso-packager.xml";
 }
