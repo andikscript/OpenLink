00de8b598d77ab61fe67f2e153c6076bcb289707
andik.saputro <andik.saputro@pactindo.com>
2022-10-01 10:29:17 +0700

    add module core, iacss, atm, global

--- src/main/java/com/andikscript/igate/value/Module.java
+++ src/main/java/com/andikscript/igate/value/Module.java
@@ -5,4 +5,10 @@ public class Module {
     public final static String ib = "ib";
 
     public final static String itm = "itm";
+
+    public final static String atm = "atm";
+
+    public final static String core = "core";
+
+    public final static String global = "global";
 }
