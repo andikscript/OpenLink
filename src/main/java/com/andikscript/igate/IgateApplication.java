7ff4bf36db06315d5c7e29bae5ef79e0511b19e6
andik.saputro <andik.saputro@pactindo.com>
2022-10-01 10:28:42 +0700

    remove entered field

--- src/main/java/com/andikscript/igate/IgateApplication.java
+++ src/main/java/com/andikscript/igate/IgateApplication.java
@@ -1,7 +1,10 @@
 package com.andikscript.igate;
 
+import com.andikscript.igate.socket.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class IgateApplication {
@@ -9,5 +12,4 @@ public class IgateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IgateApplication.class, args);
 	}
-
 }
